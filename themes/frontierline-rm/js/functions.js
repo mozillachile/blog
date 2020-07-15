ebb901ad5c2bceb5c9da0491687e42a6e0f8dcd2
iformas <iformas@hotmail.com>
2020-07-15 04:03:36 -0400

    instant feed + fixes

--- themes/frontierline-rm/js/functions.js
+++ themes/frontierline-rm/js/functions.js
@@ -1,6 +1,5 @@
-(function() {
+$(function() {
     'use strict';
-
 
     var $window = $(window);
     var navMoz = $('#nav-mozilla-menu');
@@ -11,7 +10,6 @@
     var siteWrap = $('.site-wrap');
     var canStick = $('.can-stick');
     var topLink = $('.page-top');
-
 
     // Sticky navigation
     var fixed = false;
@@ -82,4 +80,4 @@
    
 	console.log("             _.-~-.\n           7''  Q..\\\n        _7         (_\n      _7  _/    _q.  /\n    _7 . ___  /VVvv-'_                                            .\n   7/ / /~- \\_\\\\      '-._     .-'                      /       //\n  ./ ( /-~-/||'=.__  '::. '-~'' {             ___   /  //     ./{\n V   V-~-~| ||   __''_   ':::.   ''~-~.___.-'' _/  // / {_   /  {  /\n  VV/-~-~-|/ \\ .'__'. '.    '::                     _ _ _        ''.\n  / /~~~~||VVV/ /  \\ )  \\        _ __ ___   ___ ___(_) | | __ _   .::'\n / (~-~-~\\\\.-' /    \\'   \\::::. | '_ ` _ \\ / _ \\_  / | | |/ _` | :::'\n/..\\    /..\\__/      '     '::: | | | | | | (_) / /| | | | (_| | ::'\nvVVv    vVVv                 ': |_| |_| |_|\\___/___|_|_|_|\\__,_| ''\n\n");
 		
-})(jQuery);
+});
